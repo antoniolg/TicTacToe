7ce71e1205058f4f2ecde97145210705b62326b3
antoniolg <antonio@devexperto.com>
2023-03-28 11:22:12 +0200

    Configure project to start using multiplatform

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,18 @@
+group "com.antonioleiva"
+version "1.0-SNAPSHOT"
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
 plugins {
-    id("com.android.application") version "7.4.0-beta02" apply false
-    id("com.android.library") version "7.4.0-beta02" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    kotlin("multiplatform") apply false
+    kotlin("android") apply false
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    id("org.jetbrains.compose") apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,16 +3,19 @@
 pluginManagement {
     repositories {
         google()
-        mavenCentral()
         gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+
+    plugins {
+        kotlin("multiplatform").version(extra["kotlin.version"] as String)
+        kotlin("android").version(extra["kotlin.version"] as String)
+        id("com.android.application").version(extra["agp.version"] as String)
+        id("com.android.library").version(extra["agp.version"] as String)
+        id("org.jetbrains.compose").version(extra["compose.version"] as String)
     }
 }
+
 rootProject.name = "TicTacToe"
 include(":app")
