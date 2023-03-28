9a839d01c2dc3e613d6de0d687363554f241d620
antoniolg <antonio@devexperto.com>
2023-03-28 11:09:10 +0200

    Convert to build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    id("com.android.application") version "7.4.0-beta02" apply false
+    id("com.android.library") version "7.4.0-beta02" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+}
